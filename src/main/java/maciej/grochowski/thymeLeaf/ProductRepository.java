c7c488b6cead58f82e9492554bb3b3d676b43287
Maciej <maciejgrochowski93@o2.pl>
2021-04-28 15:52:01 +0200

    the first commit

--- src/main/java/maciej/grochowski/thymeLeaf/ProductRepository.java
+++ src/main/java/maciej/grochowski/thymeLeaf/ProductRepository.java
@@ -1,8 +1,6 @@
 package maciej.grochowski.thymeLeaf;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
